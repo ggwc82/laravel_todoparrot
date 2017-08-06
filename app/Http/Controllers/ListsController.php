1a754ed084af77a9014e632b0c63ed7cac0b24ad
Godfrey <ggc100@godfreys-air.home>
2017-08-06 16:51:53 +0100

    implemented first RESTful laravel controller

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -13,7 +13,7 @@ class ListsController extends Controller
      */
     public function index()
     {
-        //
+        return view('lists.index');
     }
 
     /**
---
+++ resources/views/lists/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Lists</h1>
+
+@endsection
